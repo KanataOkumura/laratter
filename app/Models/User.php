fe854c3d2af44743c5df1fac8021c8ed714f24d8
KanataOkumura <okumura.kanata.971@s.kyushu-u.ac.jp>
2024-09-15 12:47:13 +1000

    Like

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,11 @@ class User extends Authenticatable
         ];
     }
     public function tweets()
-  {
-    return $this->hasMany(Tweet::class);
-  }
+    {
+     return $this->hasMany(Tweet::class);
+    }
+    public function likes()
+    {
+     return $this->belongsToMany(Tweet::class)->withTimestamps();
+    }
 }
